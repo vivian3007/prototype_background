f0c279560b817c4161f0c8b1aa5d5cf37f1fe7d8
vivian <1022620@hr.nl>
2022-06-02 11:34:57 +0200

    background

---
+++ src/game.ts
@@ -0,0 +1,30 @@
+import * as PIXI from 'pixi.js'
+import backgroundImage from './images/background.png'
+
+export class Game {
+    pixi: PIXI.Application
+    loader: PIXI.Loader
+
+    private pixiWidth = window.innerWidth
+    private pixiHeight = window.innerHeight
+
+    constructor() {
+        this.pixi = new PIXI.Application({ width: this.pixiWidth, height: this.pixiHeight })
+        document.body.appendChild(this.pixi.view)
+
+        this.loader = new PIXI.Loader()
+        this.loader.add('backgroundTexture', backgroundImage)
+        this.loader.load(() => this.loadCompleted())
+    }
+
+    loadCompleted(){
+        //create background
+        let background = new PIXI.Sprite(this.loader.resources["backgroundTexture"].texture!);
+        background.height = this.pixiHeight;
+        background.width = this.pixiWidth;
+        this.pixi.stage.addChild(background);
+    }
+
+}
+
+let game = new Game()
